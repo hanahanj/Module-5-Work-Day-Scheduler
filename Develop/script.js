3499f6aa647c9da284855ad9697d66cdc622c79c
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2024-02-01 10:55:49 -0600

    display current date

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(function(){
+var today = dayjs();
+var time = dayjs().format('hh:mm:ss');
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +24,14 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  
+  $('#currentDay').text(today.format('MMM D, YYYY'));
+  $('#currentTime').text(time);
+
+  
+ 
+
+
+});
+
 });
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Module-5-Work-Day-Scheduler
 Bootcamp Module 5 Challange: Work Day Scheduler
+
+
