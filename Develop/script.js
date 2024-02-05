a64ebbf311b08d5804b50beb81d119afdde0295d
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2024-02-05 08:42:08 -0600

    updated local storage set/get to load items when refreshed

--- Develop/script.js
+++ Develop/script.js
@@ -3,55 +3,57 @@
 // in the html.
 
 
-
-
 $(document).ready(function(){
   var today = dayjs();
   var time = dayjs().format('hh'); 
   var saveBtn = $('.saveBtn');
+
   var past = $('.past');
   var present = $('.present');
   var future = $('.future');
 
-  $(function () {
-
+  // Display current date
   $('#currentDay').text(today.format('MMM D, YYYY'));
   // $('#currentTime').text(time);
   console.log(time);
 
-  
-    
-  
-  
-
-  // TODO: Add a listener for click events on the save button.
+  //  Add a listener for click events on the save button.
   saveBtn.on('click', function () {
-    // get the specific time slot
-    var timeSlot = $(this).parent().attr('id');
+    
+    // check local storage and see if there is anything alread saved
+    var storedTimeBlocks = JSON.parse(localStorage.getItem('timeSlotsKey'));
 
-    // Log any input in this tmeslot to local storage
+    // get the specific time slot for the entry that was saved
+    var timeBlock = $(this).parent().attr('id');
+
+    // Log the data in the description field to local storage
     var entry = $(this).siblings(".description").val();
 
-    localStorage.setItem("entry " + timeSlot, JSON.stringify(entry));
+ 
+// if entries are already stored in local Storage, add the new saved item to the existing object
+    if (storedTimeBlocks !=null){
+     
+      storedTimeBlocks [timeBlock] = entry;
+        
+     
+    } else {
+      // if there is nothing in local Storage, make a new object
+      storedTimeBlocks={}
+      storedTimeBlocks [timeBlock] = entry;
+    }
 
-
+    // save to  local storage
+    localStorage.setItem("timeSlotsKey", JSON.stringify(storedTimeBlocks));
+    
 
   });
-
-
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  
 
  // Military time threshold to confirm if the timeslot is past, present, future
  var milTime = dayjs().format('HH');
  console.log("military time " + milTime); 
 
-
+//  apply the past, present, or future class to each time block by comparing the id to the current hour.
  const divs = document.querySelectorAll('div');
 
  divs.forEach(div =>{
@@ -79,33 +81,29 @@ $(document).ready(function(){
  });
 
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
- 
-  
+
+// get any user input that was saved in localStorage and set
+//   the values of the corresponding textarea elements. HINT: How can the id
+//   attribute of each time-block be used to do this?
+
+function loadFromLocalStorage (){
+var storedTimeBlocks = JSON.parse(localStorage.getItem('timeSlotsKey'));
+  console.log(storedTimeBlocks);
+
+if (storedTimeBlocks != null ){
+  console.log(storedTimeBlocks);
+
+  for (let timeBlock in storedTimeBlocks){
+  console.log(timeBlock); 
+  document.getElementById(timeBlock).children[1].value=storedTimeBlocks[timeBlock];
+
+  }
 
 
-  // TODO (Extra): Get current time to display and realtime update.
-
-
-});
-
-function display() {
-  $('.time-block').each(function() {
-    var timeSlot = $(this).attr('id');
-    var logEntry = localStorage.getItem(timeSlot);
-
-    if (logEntry !== null) {
-      $(this).children('.description').val(JSON.parse(logEntry));
-      console.log("Entries Written to Blocks");
-    }
-  });
+}
 }
 
-display();
+loadFromLocalStorage();
+
 
 });
